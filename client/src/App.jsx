b5f966ef26316650d9c123ea35a8677e54f0cec6
Kat <78310932+Krekensis@users.noreply.github.com>
2025-06-29 19:09:48 +0530

    added vercel analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,19 @@ import Home from "./pages/HomePage";
 import ItemValueVisualizer from "./pages/ItemValueVisualizer";
 import AllItemsOverview from "./pages/AllItemsOverview";
 import ItemMarketVisualizer from "./pages/ItemMarketVisualizer";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/item-value-visualizer" element={<ItemValueVisualizer />} />
-      <Route path="/all-items-overview" element={<AllItemsOverview/>} />
-      <Route path="/item-market-visualizer" element={<ItemMarketVisualizer/>} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/item-value-visualizer" element={<ItemValueVisualizer />} />
+        <Route path="/all-items-overview" element={<AllItemsOverview />} />
+        <Route path="/item-market-visualizer" element={<ItemMarketVisualizer />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 };
 
